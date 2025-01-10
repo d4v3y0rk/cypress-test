205dcfd5062bd577c168d61a2d2f07c4b7672123
Dave York <dyork@brainchain.ai>
2025-01-10 04:51:12 -0500

    try another way to test

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,6 +1,6 @@
 describe('Cypress Test', () => {
-  it('should visit the Cypress website', () => {
-    cy.visit('https://www.cypress.io');
+  it('should visit the local test page', () => {
+    cy.visit('cypress/static/index.html');
     cy.contains('JavaScript End to End Testing Framework');
   });
 });
